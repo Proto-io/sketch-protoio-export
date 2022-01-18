dfa9a68c4f25f7e52ba83042097ea7fa15917b5e
Angelos Constantinou <angelosc@proto.io>
2022-01-18 12:16:25 +0200

    Bug fixes

--- Export to Proto.io.sketchplugin/Contents/Sketch/export_all.js
+++ Export to Proto.io.sketchplugin/Contents/Sketch/export_all.js
@@ -34,7 +34,7 @@ var currentArtboard;
 var duplicateFileNameWarning=false;
 var apVersion=true;
 var minimalExportMode=true;
-var version="1.29";
+var version="1.30";
 var debugMode=false;
 var export_scale=1.0;
 var exportSelectedItemsOnly=false;
